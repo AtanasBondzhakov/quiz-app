7e186746632b0a31900b6bd0027de01e04146bd2
atanasbondzhakov <atanas.b@yahoo.com>
2025-04-23 11:04:34 +0300

    Create QuestionTimer component

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function QuestionTimer({
+    timeout,
+    onTimeout
+}) {
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        setTimeout(onTimeout, timeout);
+    }, [onTimeout, timeout]);
+
+    useEffect(() => {
+        setInterval(() => {
+            setRemainingTime(prevState => prevState - 100);
+        }, 100);
+    }, []);
+
+    return (
+        <progress
+            id="question-time"
+            max={timeout}
+            value={remainingTime}
+        />
+    );
+};
