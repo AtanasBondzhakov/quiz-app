54ea152294b5fad1b613f57d03e26c2fe5b7e7db
atanasbondzhakov <atanas.b@yahoo.com>
2025-04-23 09:58:39 +0300

    Add summary screen and shuffle possible answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import QUESTIONS from '../questions.js';
+import completeImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAnswers(prevState => (
@@ -13,12 +15,24 @@ export default function Quiz() {
         ));
     };
 
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={completeImg} alt="Trophy Image" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
